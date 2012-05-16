6a95063b4a72cce9ee895c86f478e2cf312224d6
skyline199269 <skyline199269@47262337-c4a5-35e6-8ebf-61ebc4cc8000>
2012-05-16 04:05:45 +0000

    Primeiro Commit !!!!

---
+++ GFP/src/com/name/gfp/GFPActivity.java
@@ -0,0 +1,13 @@
+package com.name.gfp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GFPActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ GFP/src/com/name/gfp/GerenciadorFinanceiro.java
@@ -0,0 +1,35 @@
+package com.name.gfp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Classe responsavel pelo gerenciamento financeiro do usuario
+ * 
+ * @author ARTHUR SENA, RODOLFO LIMA, BRUNNA AMORIM, ELIAS PAULINO
+ */
+
+public class GerenciadorFinanceiro {
+	
+	List<Transacao> listaDeTransacoes;
+	
+	/**
+	 * Construtor da Classe
+	 */
+	
+	public GerenciadorFinanceiro(){
+		listaDeTransacoes = new ArrayList<Transacao>();
+	}
+	
+	/**
+	 * Recupera a Lista de Transacoes realizada pelo Usuario
+	 * @return
+	 *        Lista de Transacoes 
+	 */
+	
+	public List<Transacao> getListaDeTransacoes() {
+		return listaDeTransacoes;
+	}
+	
+	
+}
